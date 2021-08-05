5ab205b24ba778c2b6cc94b79c22e1940e9fa3ca
ohadApiiro <ohad@apiiro.com>
2021-08-05 14:58:15 +0300

    dum

--- PyDataModels/AttrsDataModel.py
+++ PyDataModels/AttrsDataModel.py
@@ -1,7 +1,20 @@
 from attr import attrs, attrib
 
+from PyDataModels.BarFile import Bar
+from PyDataModels.FooFile import Foo
+
 
 @attrs
 class AttrDataModel:
     product_name = attrib()
     price = attrib()
+
+bar = Bar()
+
+bar.bar_func()
+def global_func(bla):
+    pass
+
+
+foo = Foo()
+global_func(foo)
---
+++ PyDataModels/BarFile.py
@@ -0,0 +1,9 @@
+from PyDataModels.FooFile import Foo
+
+
+class Bar(Foo):
+    def __init__(self):
+        self.bar_var = 2
+
+    def bar_func(self):
+        print("bar")
