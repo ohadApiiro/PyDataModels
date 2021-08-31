2634d4041f003c4f2ac7c24f5b2281fb73b80bd9
ohadApiiro <ohad@apiiro.com>
2021-08-31 12:03:20 +0300

    add call to global function

--- PyDataModels/AttrsDataModel.py
+++ PyDataModels/AttrsDataModel.py
@@ -9,12 +9,14 @@ class AttrDataModel:
     product_name = attrib()
     price = attrib()
     def __init__(self):
-        bar = Bar
+        bar = Bar()
         self.x = bar.bar_func()
+        global_func(bar)
 
-def global_func(bla):
-    bar = Bar()
+
+def global_func(bar):
     bar.bar_func()
+    return 'abc'
 
 class A:
     def __init__(self):
